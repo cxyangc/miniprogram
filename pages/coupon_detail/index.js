a98774daf14a1f19313c39600d4c74ce03331ad6
赵同学 <952211783@qq.com>
2018-09-19 11:30:11 +0800

    赵-优惠券详情

---
+++ pages/coupon_detail/index.js
@@ -0,0 +1,201 @@
+ 
+const app = getApp()
+Page({ 
+
+  /**
+   * 页面的初始数据
+   */ 
+
+  data: {
+    couponDetail:null,
+    color:'#e2534d',
+    couponUserState: true,
+    couponEnableState: true,
+    couponState: true,
+    clickStyle:'receive'
+  },
+
+  
+
+  getData: function () {
+    let that = this
+    let customIndex = app.AddClientUrl("/get_coupon_detail.html", that.optParam, 'get')
+    wx.request({
+      url: customIndex.url,
+      header: app.header,
+      success: function (res) {
+        console.log(res.data);
+        that.setData({couponDetail: res.data.relateObj})
+        if (that.data.couponDetail.isusing == 1) {
+          console.log('可用')
+          that.setData({ couponEnableState: true })//1可用
+          that.setData({ color: '#e2534d' })
+
+          if (that.data.couponDetail.userGotCoupon.length != 0) {
+            that.setData({ clickStyle: 'used' })//点击类型
+            that.setData({ couponState: true })//已领取
+            wx.showToast({
+              title: '你已经领过了',
+              icon: 'success',
+              duration: 1000
+            })
+            if (that.data.couponDetail.userGotCoupon[0].isUsed == 0) {
+              that.setData({ couponUserState: true })//0未使用
+              that.setData({ color: '#e2534d' })
+            } else {
+              that.setData({ couponUserState: false })//已使用
+              that.setData({ color: '#E7E7E7' })
+              that.setData({ clickStyle: 'noClick' })//点击类型
+            }
+          } else {
+            that.setData({ couponState: false })//未领取
+            that.setData({ clickStyle: 'receive' })
+          }
+
+
+        } else {
+          console.log('不可用')
+          that.setData({ couponEnableState: false })//不可用
+          that.setData({ color: '#E7E7E7' })
+          that.setData({ clickStyle: 'noClick' })//点击类型
+        }
+        console.log(that.data.couponDetail)
+        wx.hideLoading()
+      },
+      fail: function (res) {
+        console.log('fail')
+        wx.hideLoading()
+        app.loadFail()
+      }
+    })
+  },
+  selectFun:function(){
+    let that=this;
+    if (that.data.clickStyle =='receive'){
+      that.gainCoupon();
+    } else if (that.data.clickStyle == 'used'){
+      app.toIndex();
+    }else{
+      console.log('不可点击');
+      return;
+    }
+  },
+  gainCoupon: function () {
+    let that = this
+    let customIndex = app.AddClientUrl("/gain_coupon.html", that.optParam, 'post')
+    console.log("customIndex", customIndex);
+    wx.request({
+      url: customIndex.url,
+      data: customIndex.params,
+      method: 'POST',
+      header: app.headerPost,
+      success: function (res) {
+        console.log(res.data);
+        if (res.data.errcode && res.data.errcode==-1){
+          wx.showToast({
+            title: res.data.relateObj,
+            icon: 'success',
+            duration: 1000
+          })
+        }else{
+          if (res.data.newGot == 0 && res.data.otherGot == 0) {
+            wx.showToast({
+              title: '你已经领过了',
+              icon: 'success',
+              duration: 1000
+            })
+          }
+          else if (res.data.otherGot == 1) {
+            wx.showToast({
+              title: '已被别人领取',
+              icon: 'success',
+              duration: 1000
+            })
+          }
+          else if (res.data.newGot == 0) {
+            wx.showToast({
+              title: '领取成功',
+              icon: 'success',
+              duration: 1000
+            })
+          }
+        }
+        wx.hideLoading()
+      },
+      fail: function (res) {
+        console.log('fail')
+        wx.hideLoading()
+        app.loadFail()
+      }
+    })
+  },
+
+  /* 组件事件集合 */
+  tolinkUrl: function (e) {
+    let linkUrl = e.currentTarget.dataset.link
+    app.linkEvent(linkUrl)
+  },
+  toCouponList: function (event) {
+    console.log("--------toCouponList------")
+    wx.navigateTo({
+      url: '../../pages/my_coupons/index',
+    })
+  },
+  gotoGetCoupon: function () {
+    wx.navigateTo({
+      url: '../available_coupons/index',
+    })
+  },
+  optParam: {}, // option数据 用来转发和刷新
+  onLoad: function (options) {
+    console.log("===options===",options)
+    this.optParam = options
+    this.getData(options)
+  },
+
+  onReady: function () {
+    this.setData({
+      setting: app.setting,
+      loginUser: app.loginUser,
+      sysWidth: app.globalData.sysWidth,
+      sysHeight: app.globalData.sysHeight,
+    });
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+    this.data.List = {
+      tab: [],
+      listData: []
+    }
+    this.onLoad()
+
+    wx.stopPullDownRefresh()
+  },
+
+  
+})
