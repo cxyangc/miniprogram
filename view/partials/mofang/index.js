0b010f0468201194befa3ad715ef9409b5139a7c
赵同学 <952211783@qq.com>
2018-10-09 17:48:31 +0800

    zhao

---
+++ view/partials/mofang/index.js
@@ -0,0 +1,25 @@
+const app = getApp()
+Component({
+  properties: {
+    // 这里定义了innerText属性，属性值可以在组件使用时指定
+    data: {
+      type: JSON,
+    },
+  },
+  data: {
+    // 这里是一些组件内部数据
+   
+
+  },
+  ready:function(){
+    console.log('=====ready====', this.data.data)
+    this.setData({ items: this.data.data.jsonData.items })
+    this.setData({ width: this.data.data.jsonData.width })
+    this.setData({ height: this.data.data.jsonData.height })
+    this.setData({ imagePadding: this.data.data.jsonData.imagePadding })
+    console.log('=====items====', this.data.items)
+  },
+  methods: {
+    
+  }
+})
